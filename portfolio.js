29f4f170944ef46f3c0e69c2ec817f837c735f2a
Menen <szymonmenen1@gmail.com>
2023-02-26 23:09:50 +0100

    proto_def

--- portfolio.js
+++ portfolio.js
@@ -1,2 +1,27 @@
+//Fonctionnement de la modal du portfolio
+//Ce code utilise la méthode document.querySelectorAll() pour pointer sur tout les bouton de la classe project
+//qui ont la classe CSS "project" et les stocke dans une variable appelée buttons.
+
+const buttons = document.querySelectorAll('.project');
+const overlay = document.querySelector('.overlay');
+const overlayImage = document.querySelector('.overlay__inner img');
+const description = document.querySelector('description p');
+
+
+
+function open(e) {
+  overlay.classList.add('open');
+  const src= e.currentTarget.querySelector('img').src;
+  overlayImage.src = src;
+  
+}
+
+function close() {
+  overlay.classList.remove('open');
+
+}
+
+buttons.forEach(button => button.addEventListener('click', open));
+overlay.addEventListener('click', close);
 
 
